f5c48a24921ecac73ffd5e168bda4540d1aa30d2
andhale-pranav <pranav.22010461@viit.ac.in>
2024-10-22 02:12:22 +0530

    Album class completed

--- src/spotifyclone/Album.java
+++ src/spotifyclone/Album.java
@@ -1,6 +1,7 @@
 package spotifyclone;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Album {
@@ -15,6 +16,76 @@ public class Album {
         this.albumName = albumName;
         this.artistName = artistName;
         this.songList = new ArrayList<>();
+
     }
+
+    //find song in album
+    //taking input of song name, returning boolean
+    public boolean findSongInAlbum(String title) {
+
+        for(Song song : songList) {
+            if(song.name.equals(title)) return true;
+        }
+
+        return false;
+    }
+
+    //add song to album
+    //input of songname and duration
+    //first check if the song is present in songlist
+    //if not, create new obj and add to songList
+    //if already present, return the msg
+    public String addSongToAlbum(String title, double duration) {
+
+        if(findSongInAlbum(title)){
+            return "Song " + title + " is already present in the playlist";
+        }
+
+        Song newSong = new Song(title, duration);
+        songList.add(newSong);
+        return "Song " + title + " is added to the playlist";
+
+    }
+
+    //add song to playlist - with index
+    //input of index and playlist name
+    //check if index is valid
+    //add song if valid
+    public String addSongToPlayList(int trackNo, LinkedList<Song> playList){
+
+        int idx = trackNo - 1;
+
+        if(idx >= 0 && idx < songList.size()) {
+            Song currSong = songList.get(idx);
+            playList.add(currSong);
+            return "Song " + currSong.name + " with track number " +trackNo+ " added to the playlist " + playList ;
+        }
+
+        return "Invalid track number";
+
+    }
+
+    //add song to playlist - with name
+    //input of name and playlist
+    //check if song exists
+    //add
+    public String addSongToPlayList(String title, LinkedList<Song> playList) {
+
+        if(findSongInAlbum(title)){
+            return "Song " + title + " is already present in the playlist";
+        }
+
+        for(Song song : songList) {
+            if(song.name.equals(title)){
+                playList.add(song);
+                return "Song " + song.name + " added to the playlist " + playList ;
+            }
+        }
+
+        return "this is not supposed to happen";
+    }
+
+
+
 
 }
--- src/spotifyclone/Main.java
+++ src/spotifyclone/Main.java
@@ -1,9 +1,48 @@
 package spotifyclone;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+
+    public static List<Album> albums = new ArrayList<>();
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+
+//        SpiralMatrix sm = new SpiralMatrix();
+//        sm.printMatrix(3, 3);
+        System.out.println("Hello world!");
+
+        Album album = new Album("Old Hindi Songs","Arijit Singh");
+
+        album.addSongToAlbum("Pathaan",4.5);
+        album.addSongToAlbum("Channa mereya",3.5);
+        album.addSongToAlbum("Naina",5.0);
+
+        albums.add(album);
+
+        album = new Album("New Songs","Prateek Kuhad");
+
+        album.addSongToAlbum("Kasoor",4.5);
+        album.addSongToAlbum("Tum mile",3.5);
+        album.addSongToAlbum("Baarishein",4.5);
+
+        albums.add(album);
+
+        LinkedList<Song> playList_1 = new LinkedList<>();
+
+        albums.get(0).addSongToPlayList("Pathaan",playList_1);
+        albums.get(0).addSongToPlayList("Naina",playList_1);
+        albums.get(1).addSongToPlayList("Kasoor",playList_1);
+        albums.get(1).addSongToPlayList("Baarishein",playList_1);
+
+        //print the playList that has been added
+        //play(playList_1);
+
+
+
     }
 }
